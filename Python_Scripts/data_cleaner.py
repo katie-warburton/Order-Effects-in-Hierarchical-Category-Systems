80e44cef2e8ed83ba02cca3555350486e648c9b8
Katie Warburton <kwarburton@cs.toronto.edu>
2025-12-08 14:59:15 +1100

    condense together code for extracting data from different prolific pools

--- Python_Scripts/data_cleaner.py
+++ Python_Scripts/data_cleaner.py
@@ -7,7 +7,6 @@ DISTRACTORS = ['1', '3.01', '29.01', '31']
 D, ITEM_HASH = OCS.get_distance_mat([i for i in range(1, 32)])
 ITEM_HASH[3.01] = 2
 ITEM_HASH[29.01] = 28
-print(ITEM_HASH)
 
 def get_demographics(trial):
     demo = {'part': 'demographics'}
@@ -27,7 +26,10 @@ def get_experimental_trials(data, prefix):
         attempts = 0
         pid = f'{prefix}{i:03d}'
         experiment_trials[pid] = []
+        if prefix == 'rep':
+            pass
         for trial in participant['results']:
+            
             if 'part' in trial.keys() and trial['part'] == 'experiment':
                 experiment_trials[pid].append(trial)
             elif 'part' in trial.keys() and trial['part'] == 'demographics':
@@ -52,7 +54,6 @@ def check_distractors(cat_choices):
         incorrect += 1
     return incorrect
 
-
 def find_in_tree(cat, item):
     if len(cat['children']) > 0:
         for child in cat['children']:
@@ -72,30 +73,38 @@ def get_missing_label(trial_row, final_tree):
     return trial_row
 
 def extract_dfs(exp_data):
-    trial_data = []
-    participant_data = []
-    sequence_data = []
-    # trees = {}
+    trial_data, participant_data, sequence_data = [], [], []
     for pid, trials in exp_data.items():
+        if trials[0]['age'] < 18: # everyone should be under 18, but just in case
+            continue
         total_errors = 0
-        if pid[0] == 'p':
-            pool = 'prolific'
-        elif pid[0] == 'r':
+        if pid[0:3] == 'p1_':
+            pool = 'prolific1'
+        elif pid[0:3] == 'p2_':
+            pool = 'prolific2'
+        elif pid[0:3] == 'r1_':
             pool = 'rep'
-        else:
+        else: # shouldn't get here with actual experimental data 
             pool = 'test'
         participant_row = {'P_ID': pid}
         for tr in trials[1:-2]:
             if tr['part'] == 'experiment':
                 d, l, o = list(tr['condition'])
                 trial_row = tr['category_choices']
-                # if o.lower() in ['f', 'b', 'm']:
-                #     sequence_row = {f't{i+1:02}': it for i, it in enumerate(tr['item_order'])}
-                #     print(sequence_row)
-                # else:
-                #     sequence_row = {} # should it be order things are added to the tree??
-                sequence_row = {f't{i+1:02}': it for i, it in enumerate(tr['item_order'])} # still not sure the best way to handle sequence for all at once??
-                trial_row = {item: cat for pair in trial_row for item, cat in pair.items()}
+                if pool == 'prolific2':
+                    # if o.lower() in ['f', 'b', 'm']:
+                    #     sequence_row = {f't{i+1:02}': it for i, it in enumerate(tr['item_order'])}
+                    #     print(sequence_row)
+                    # else:
+                    #     sequence_row = {} # should it be order things are added to the tree??
+                    sequence_row = {f't{i+1:02}': it for i, it in enumerate(tr['item_order'])} # still not sure the best way to handle sequence for all at once??
+                    trial_row = {item: cat for pair in trial_row for item, cat in pair.items()}
+                    sequence_row['P_ID'] = pid
+                    sequence_row['DEPTH'] = d
+                    sequence_row['LOC'] = l
+                    sequence_row['ORDER'] = o.lower()
+                    sequence_row['STIMULI'] = tr['stimuli'][-1]
+                    sequence_data.append(sequence_row)
                 if None in trial_row.values():
                     trial_row = get_missing_label(trial_row, tr['final_tree'])
                 errors = check_distractors(trial_row)
@@ -106,19 +115,11 @@ def extract_dfs(exp_data):
                 trial_row['STIMULI'] = tr['stimuli'][-1]
                 trial_row['ERRORS'] = errors
                 trial_row['POOL'] = pool
-            
                 tree = OCS.CategorySystem(ITEM_HASH)
                 tree.root = tree.parse_cats(tr['final_tree'])
                 score = OCS.ordered_CKMM(tree.root, D)
                 trial_row['SCORE'] = score
                 trial_data.append(trial_row)
-
-                sequence_row['P_ID'] = pid
-                sequence_row['DEPTH'] = d
-                sequence_row['LOC'] = l
-                sequence_row['ORDER'] = o.lower()
-                sequence_row['STIMULI'] = tr['stimuli'][-1]
-                sequence_data.append(sequence_row)
                 total_errors += errors
         participant_row['GENDER'] = trials[0]['gender']
         participant_row['AGE'] = trials[0]['age']
@@ -129,17 +130,17 @@ def extract_dfs(exp_data):
         participant_data.append(participant_row)
     # make and save participant data frame
     participant_data = pd.DataFrame.from_dict(participant_data)
-    participant_data.to_csv('Results/participant_data_v2.csv', index=None)
+    participant_data.to_csv('Results/participant_data.csv', index=None)
     # make an save trial data in a data frame
     trial_data = pd.DataFrame.from_dict(trial_data)
     trial_data = trial_data[['P_ID', 'DEPTH', 'LOC', 'ORDER'] + [f'{i}' for i in range(9, 24)] + DISTRACTORS + ['ERRORS', 'STIMULI', 'POOL', 'SCORE']]
     trial_data.rename(columns={f'{i}':f'I{i:02}' for i in range(9, 24)}, inplace=True)
     trial_data.rename(columns={'1': 'I01', '3.01': 'I03', '29.01': 'I29', '31': 'I31'}, inplace=True)
-    trial_data.to_csv('Results/trial_data_v2.csv', index=None)
-    #make and save sequence data frame
+    trial_data.to_csv('Results/trial_data.csv', index=None)
+    # make and save sequence data if included
     sequence_data = pd.DataFrame.from_dict(sequence_data)
     sequence_data = sequence_data[['P_ID', 'DEPTH', 'LOC', 'ORDER'] + [f't{i:02}' for i in range(1, 14)] + ['STIMULI']]
-    sequence_data.to_csv('Results/sequence_data_v2.csv', index=None)
+    sequence_data.to_csv('Results/sequence_data.csv', index=None)
 
 def remove_duplicates(data):
     seen = set()
@@ -162,14 +163,22 @@ def remove_duplicates(data):
         no_dupes.append(first_attempt)
     return no_dupes
        
-def main(filepath, specific_ids=[]):
+def main(filepath):
     with open(filepath, 'r') as f:
         data = json.load(f)
     # specific uid is used if errors in normal way of storing data 
-    prolific = [d for d in data if ('prolific_id' in d.keys() and d['study_id'] == '692d097af153eb01f86589b3') 
-                or (d['uid'] in specific_ids)]
-    prolific= get_experimental_trials(prolific, 'p')
+    prolific1 = [d for d in data if ('prolific_id' in d.keys() and d['study_id'] == '6865dd825bc59eb3524941fb') 
+                or (d['uid'] in ['bdd4194403da271bff66e4e237429bdd'])]
+    prolific1 = get_experimental_trials(prolific1, 'p1_')
 
-    extract_dfs(prolific)   
+    rep = [d for d in data if 'REP' in d.keys()]
+    rep = remove_duplicates(rep)
+    rep = get_experimental_trials(rep, 'r1_')
 
-main('Results/results-98863bd139ec98cf6bc52549beaaf679-2025-12-04-00-39-56.json')
+    prolific2 = [d for d in data if ('prolific_id' in d.keys() and d['study_id'] == '692d097af153eb01f86589b3') 
+                or (d['uid'] in ['a818665ec602ae819875f02f1feb3d7a'])]
+    prolific2 = get_experimental_trials(prolific2, 'p2_')
+
+    extract_dfs(prolific1 | rep | prolific2)   
+
+main('Results/results-98863bd139ec98cf6bc52549beaaf679-2025-12-07-23-58-07.json')

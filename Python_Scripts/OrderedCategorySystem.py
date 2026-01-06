7d98b6368fe60293f2cebdfd544cbec9e4db5aeb
Katie Warburton <kwarburton@cs.toronto.edu>
2026-01-06 14:56:37 +1100

    Updates to code for model fitting + data simulation.

--- Python_Scripts/OrderedCategorySystem.py
+++ Python_Scripts/OrderedCategorySystem.py
@@ -164,7 +164,7 @@ def softmax(x, temp):
     e_x = np.exp(x)
     return e_x / np.sum(e_x, axis=-1, keepdims=True)
 
-def greedy_categorizer_softmax(best_syst, item_seq, D, treeLookup=None, temp=1):
+def greedy_categorizer_softmax(best_syst, item_seq, D, treeLookup=None, temp=1, alpha=0):
     cat_choices = {}
     if treeLookup is None:
         treeLookup = defaultdict(lambda: None)
@@ -184,7 +184,9 @@ def greedy_categorizer_softmax(best_syst, item_seq, D, treeLookup=None, temp=1):
             sys_scores.append(ordered_CKMM(test_syst.root, D, treeLookup))
             potential_systs.append(test_syst)
             cats.append(cat.label)
-        prob_dist = softmax(np.array(sys_scores), temp)
+        choice_prob =  softmax(np.array(sys_scores), temp)
+        random = 1/len(sys_scores)
+        prob_dist = ((1-alpha) * choice_prob) + (alpha*random)
         best_idx = np.random.choice([ix for ix in range(len(potential_systs))], p=prob_dist)
         best_syst = potential_systs[best_idx]
         cat_choices[item] = cats[best_idx]
@@ -209,7 +211,7 @@ def get_distance_mat(items, min_it=None, max_it=None, noise=0):
     return D, item_hash
 
 
-def compute_possible_scores(trials, item_space, folder='Python_Scripts/BaseSystems'):
+def precompute_possible_scores(trials, item_space, folder='Python_Scripts/BaseSystems'):
     D, item_hash = get_distance_mat(item_space)
     lookUpTree = defaultdict(lambda: None)
     all_data = {tr['P_ID']: [] for tr in trials}
@@ -260,11 +262,10 @@ def get_participant_loglike(data, determ=False, temp=1.0, alpha=0.0):
             probs = softmax(sys_scores_array, temp)
         K = probs.shape[1] 
         choice_prob = probs[np.arange(probs.shape[0]), choice_indices]
-        random_baseline = np.full_like(choice_prob, 1.0 / K)
-
+        # random_baseline = np.full_like(choice_prob, 1.0 / K)
         # might want to come up with a way to fit it to two-level probabilities
     
-        p_choice = ((1 - alpha) * choice_prob) + (alpha * random_baseline)
+        p_choice = ((1 - alpha) * choice_prob) + (alpha * (1.0 / K))
         total_log_like += np.sum(np.log(p_choice))
     return total_log_like
 
